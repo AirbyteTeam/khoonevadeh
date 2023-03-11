b856d405c391e3503ae5b3945e84568bc22ad921
Milad Zare <themiladzare@gmail.com>
2023-03-11 19:48:36 +0330

    edit 2

--- src/Components/Home/MainPage/HeroArea.jsx
+++ src/Components/Home/MainPage/HeroArea.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from "./Header";
 import heroLine2 from '../../../assets/img/hero/hero-line-3.png'
 import heroTwoImg from '../../../assets/img/slider_1.jpg'
+import {Link} from "react-router-dom";
 
 function MainPage() {
 
@@ -13,15 +14,21 @@ function MainPage() {
                         <div className="col-lg-6 col-md-8 col-sm-11">
                             <div className="hero-text">
                                 <h1 className="title wow fadeInLeft" data-wow-delay="0.2s">
-                                    ما در با خانمان دست یاری شما هستیم…
+                                    ما در کارستون دست یاری شما هستیم…
                                 </h1>
                                 <p className="wow fadeInLeft text-right" data-wow-delay="0.3s">
-                                    باخانمان یک شرکت غیرانتفاعی و مستقل است. این شرکت فعالیت خود را از سال 1398 آغاز کرده است. این شرکت اولین شرکت خیریه ای در حوزه تامین مالی جمعی و تامین تخصص جمعی در ایران است. خیرین و متخصصان متناسب با توان خود در طرح های معرفی شده از این شرکت فعالیت می کنند.
+                                    کارستون یک شرکت غیرانتفاعی و مستقل است. این شرکت فعالیت خود را از سال 1398 آغاز
+                                    کرده است. این شرکت اولین شرکت خیریه ای در حوزه تامین مالی جمعی و تامین تخصص جمعی در
+                                    ایران است. خیرین و متخصصان متناسب با توان خود در طرح های معرفی شده از این شرکت
+                                    فعالیت می کنند.
                                 </p>
                                 <ul className="hero-btn">
                                     <li className="wow fadeInUp" data-wow-delay="0.4s">
-                                        <a href="#" className="main-btn">مشاهده طرح ها <i
-                                            className="far fa-arrow-left"></i></a>
+
+                                        <Link to='/projects' className="main-btn">
+                                            مشاهده طرح ها <i
+                                            className="far fa-arrow-left"></i>
+                                        </Link>
                                     </li>
                                     {/*<li className="wow fadeInUp" data-wow-delay="0.5s">
                                         <a href="#" className="video-btn"
@@ -30,7 +37,9 @@ function MainPage() {
                                 </ul>
                             </div>
                         </div>
-                        <div className="d-flex justify-content-center col-lg-6 col-md-8 col-sm-10 mx-auto wow fadeInRight" data-wow-delay="0.2s">
+                        <div
+                            className="d-flex justify-content-center col-lg-6 col-md-8 col-sm-10 mx-auto wow fadeInRight"
+                            data-wow-delay="0.2s">
                             <div className=" hero-img image-small fancy-bottom wow fadeInLeft" data-wow-delay="0.6s">
                                 <img src="http://localhost:3000/images/test.jpg" alt="Image"/>
                             </div>
